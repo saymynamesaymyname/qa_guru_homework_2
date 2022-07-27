679b5e969f1ddb74c1bc8790ff17019be56c9012
Alexander.Shestov <alexander.shestov@team.wrike.com>
2022-07-27 23:43:35 +0200

    commit class

---
+++ src/main/java/guru/qa/Cat.java
@@ -0,0 +1,20 @@
+package guru.qa;
+
+public class Cat {
+    String color;
+    String breed;
+    int age;
+
+    public Cat(String color, String breed, int age) {
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+    }
+    void tellColorAndBreed() {
+        System.out.println("Цвет кошака: " + color + ". Порода кошака:  " + breed + "." );
+    }
+
+    void tellHumanAge() {
+        System.out.println("Возраст кошака в пересчёте на человеческий: " + age*7 + ".");
+    }
+}
---
+++ src/main/java/guru/qa/First.java
@@ -0,0 +1,9 @@
+package guru.qa;
+
+public class First {
+    public static void main(String[] args) {
+    Cat redCat = new Cat("рыжий", "персидский", 3);
+    redCat.tellColorAndBreed();
+    redCat.tellHumanAge();
+}
+}
